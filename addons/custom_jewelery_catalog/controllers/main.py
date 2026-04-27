323431561e279cbe7fcd95a5f194c95b93735556
White-Vulpes <aayush.02.parmar@gmail.com>
2026-04-27 11:09:13 +0530

    added weight filter to public catalouge

--- addons/custom_jewelery_catalog/controllers/main.py
+++ addons/custom_jewelery_catalog/controllers/main.py
@@ -3,36 +3,42 @@ from odoo.http import request
 
 class JewelryCatalog(http.Controller):
     @http.route(['/catalog'], type='http', auth="public", website=True)
-    def show_catalog(self, search='', size_id='', tag_id='', **kwargs):
-        # 1. Base domain (starts empty to show all)
+    def show_catalog(self, search='', size_id='', tag_id='', min_weight='', max_weight='', **kwargs):
         domain = []
 
-        # 2. Add text search (searches Name OR Barcode)
+        # 1. Text search
         if search:
             domain += ['|', ('name', 'ilike', search), ('barcode', 'ilike', search)]
 
-        # 3. Add Size filter
+        # 2. Size & Tag filters
         if size_id:
             domain += [('size', '=', int(size_id))]
-
-        # 4. Add Tag/Design filter
         if tag_id:
             domain += [('design_tag_ids', 'in', [int(tag_id)])]
 
-        # 5. Fetch the filtered items
-        items = request.env['jewelry.barcode.item'].sudo().search(domain)
+        # 3. NEW: Weight Range Filters (using Net Weight)
+        try:
+            if min_weight:
+                domain += [('weight', '>=', float(min_weight))]
+            if max_weight:
+                domain += [('weight', '<=', float(max_weight))]
+        except ValueError:
+            # This safely ignores the filter if a user types letters into the URL by accident
+            pass 
 
-        # 6. Fetch all available Sizes and Tags to populate the dropdowns
+        # 4. Fetch Data
+        items = request.env['jewelry.barcode.item'].sudo().search(domain)
         sizes = request.env['jewelry.size'].sudo().search([])
         tags = request.env['jewelry.design.tag'].sudo().search([])
 
-        # 7. Send everything to the template
+        # 5. Send to template
         return request.render('custom_jewelery_catalog.jewelry_public_catalog', {
             'items': items,
             'sizes': sizes,
             'tags': tags,
             'search': search,
-            # We pass back the selected IDs so the dropdowns "remember" what the user picked
             'selected_size': int(size_id) if size_id else 0,
             'selected_tag': int(tag_id) if tag_id else 0,
+            'min_weight': min_weight,
+            'max_weight': max_weight,
         })
